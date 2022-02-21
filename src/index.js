2cc137e52d310b31abf556ebf9f77747e5eca501
Begerad, Stefan <dialog@swingbe.de>
2022-02-21 22:14:43 +0100

    add Javascript dependencies of Bootstrap by calling npm install jquery popper.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ import Main from './main';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+//eslint-disable-next-line
+import $ from 'jquery';
+//eslint-disable-next-line
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 ReactDOM.render(<Main />, document.getElementById('root'));
