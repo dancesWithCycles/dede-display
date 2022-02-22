f5549ba3d76af606d8fb64f20e613baf3fc6c4a6
Begerad, Stefan <dialog@swingbe.de>
2022-02-22 09:34:06 +0100

    feat: add imports according to https://getbootstrap.com/docs/5.1/getting-started/webpack/

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './main';
 
+//import 'bootstrap';
+// or get all of the named exports for further usage
+//eslint-disable-next-line
+import * as bootstrap from 'bootstrap';
+
+//eslint-disable-next-line
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 //eslint-disable-next-line
 import $ from 'jquery';
+
 //eslint-disable-next-line
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(<Main />, document.getElementById('root'));
