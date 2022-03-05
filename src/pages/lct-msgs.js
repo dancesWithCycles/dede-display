a2750d2eb68f68ae90c3f27da639fa600a5c3a65
Begerad, Stefan <dialog@swingbe.de>
2022-03-05 07:25:03 +0100

    feat: add JavaScript function getMsgs

--- src/pages/lct-msgs.js
+++ src/pages/lct-msgs.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 const LctMsgs = () => {
+    /*fetch msgs in a javascript function*/
+    const getMsgs = async () => {
+        try {
+            const msgs = await axios.get('https://dede-display.vbn.de:42001/ivu-loc');
+            /*initial log*/
+            console.log('msgs: ' + msgs.data);
+        } catch (err) {
+            console.log('err.message: ' + err.message);
+        }
+    };
+    useEffect(() => {
+    /*effect goes here*/
+    /*hook need to be placed in body of the function component in which it is used*/
+        getMsgs();
+    });
     return (
         <>
             <h2>IVU Location Messages</h2>
