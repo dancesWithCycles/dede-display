689939a442e8f5d4ff02dbe72f2330a5236d76f6
Begerad, Stefan <dialog@swingbe.de>
2022-03-07 11:20:33 +0100

    feat: save msgs as state in array and display msgs by mapping over the array

--- src/pages/lct-msgs.js
+++ src/pages/lct-msgs.js
@@ -1,26 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const LctMsgs = () => {
+    /*store msgs as array in function component state*/
+    /*initialise as empty array*/
+    const [msgs, setMsgs] = useState([]);
+
     /*fetch msgs in a JavaScript function*/
     const getMsgs = async () => {
         try {
             /*TODO handle errors: https://www.valentinog.com/blog/await-react/*/
             const msgs = await axios.get('https://dede-display.vbn.de:42001/ivu-loc');
-            /*initial log*/
-            console.log('msgs: ' + msgs.data);
+            /*set state*/
+            setMsgs(msgs.data);
         } catch (err) {
             console.log('err.message: ' + err.message);
         }
     };
+
+    /*this hook is run after a DOM update. Changing state migh result in an infinite loop*/
     useEffect(() => {
     /*effect goes here*/
-    /*hook need to be placed in body of the function component in which it is used*/
+
+        /*hook need to be placed in body of the function component in which it is used*/
         getMsgs();
-    });
+
+    /*use an empty dependency array to ensure the hook is running only once*/
+    /*TODO study dependency array: https://reactjs.org/docs/hooks-effect.html*/
+    }, []);
+
     return (
         <>
-            <h2>IVU Location Messages</h2>
+            <h1>IVU Location Messages</h1>
+            <ul>
+                {/*map over msgs array*/}
+                {msgs.map((msg) => (
+                    <li key={msg._id}>{msg.senderId}</li>
+                ))}
+            </ul>
         </>
     );
 };
